1ea809def8bedf0af30ba2d8c017c78867aff13f
charles otonda <charlesotonda@gmail.com>
2023-06-12 06:39:02 -0700

    code adjustment

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return None
+        return (None)
     if idx > (len(my_list)-1):
-        return None
+        return (None)
     for i in range(idx + 1):
         i=+i
-    return my_list[i]
+    return (my_list[i])
