1ee4aedc368ed9a2f9d9b8b8749c291d1f0445c6
charles otonda <charlesotonda@gmail.com>
2023-06-12 07:29:43 -0700

    A little adjustment

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,5 @@ def replace_in_list(my_list, idx, element):
         return (my_list)
     if idx > (len(my_list) - 1):
         return (my_list)
-    for i in range(idx + 1):
-        i += i
     my_list[idx] = element
     return (my_list)
