e12fa6caab4b197080bfb74629edd59a786240f2
charles otonda <charlesotonda@gmail.com>
2023-06-14 16:33:27 -0700

    code adjustment

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    b_dict = a_dictionary.copy()
-    for v in b_dict.keys():
-        b_dict[v] = b_dict[v] * 2
-    return (b_dict)
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+
+    return a_dictionary
