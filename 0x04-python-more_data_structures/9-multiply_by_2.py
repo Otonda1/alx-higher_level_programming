cc8fbc09e8fd6ae1258e65d5d687537e18070b18
charles otonda <charlesotonda@gmail.com>
2023-06-14 16:34:51 -0700

    adjustm,ent

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    b_dict = a_dictionary.copy()
-    for v in b_dict.keys():
-        b_dict[v] = b_dict[v] * 2
-    return (b_dict)
+    return {key: value * 2 for key, value in a_dictionary.items()}
