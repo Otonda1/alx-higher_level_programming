3bca303f30e2a9c29ef5a75882e0e8cf15639c5b
charles otonda <charlesotonda@gmail.com>
2023-07-10 14:13:06 -0700

    inheritance in python

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Retrieves all attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    Returns list of all object attributes and methods
+    """
+
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module containing empty class
+"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    pass
+
+    def area(self):
+        """Undefined method"""
+        raise Exception("area() is not implemented")
