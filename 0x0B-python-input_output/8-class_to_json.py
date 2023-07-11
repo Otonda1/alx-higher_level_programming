29819a1ad3c09779cd89c1f2f85302486099bac7
charles otonda <charlesotonda@gmail.com>
2023-07-11 15:39:52 -0700

    python input and output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""
-This module contains a python class to JSON function
-"""
+"""Contains the "class_to_json" function"""
 
 
 def class_to_json(obj):
-    """Return dictionary representation of a  simple data structure"""
-    return obj.__dict___
+    """returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object"""
+    return obj.__dict__
