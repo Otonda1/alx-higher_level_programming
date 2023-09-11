0a09b05f0e77121d4555ef1c094396adca7ed3d6
Otonda1 <charlesotonda@gmail.com>
2023-09-11 23:40:12 +0300

    a script that prints arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-	  console.log('No argument');
-} else if (process.argv.length === 3) {
-	  console.log('Argument found');
-} else if (process.argv.length > 3) {
-	  console.log('Arguments found');
+
+const process = require('process');
+const argv = process.argv;
+
+if (argv.length <= 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
