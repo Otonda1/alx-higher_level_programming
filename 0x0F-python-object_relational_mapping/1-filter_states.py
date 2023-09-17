395616992d192eb3c25e7e34dc738b88d9c1914a
charles otonda <charlesotonda@gmail.com>
2023-09-17 02:56:04 -0700

    a script that list states with letter N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    a script that lists all states with a name 
+    starting with N (upper N) from the database hbtn_0e_0_usa:
+"""
+import sys
+import MySQLdb
+
+host = 'localhost'
+
+try:
+    db = MySQLdb.connect(host=host, user=sys.argv[1], 
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='Nevada' OR  name='New York'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+except MySQLdb.Error as e:
+    print(f"errors {e} occured")
+
+
+cur.close()
+db.close()
