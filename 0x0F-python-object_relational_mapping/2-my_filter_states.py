5794270fa4907f20b9fc0c7fd50cbd0a3cbd6e44
charles otonda <charlesotonda@gmail.com>
2023-09-17 05:53:53 -0700

    a script that takes in an argument and displays all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,12 @@ if __name__ == "__main__":
         db = MySQLdb.connect(host=localhost, user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name = %s", (name,))
-        row = cur.fetchall()
-        print(row)
+        cur.execute("SELECT * FROM states WHERE\
+                    name='{:s}' ORDER BY id ASC".format(name))
+        rows = cur.fetchall()
+        for row in rows:
+            if row[1] == name:
+                print(row)
 
     except MySQLdb.Error as e:
         print(f"MySQLdb error: {e}")
