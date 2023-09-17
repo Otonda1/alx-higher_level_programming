1961adac1bba216f9f66da18a07a1a5e7e5dd51e
charles otonda <charlesotonda@gmail.com>
2023-09-17 06:43:52 -0700

    a script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+    a script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    try:
+        cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities INNER JOIN states ORDER BY\
+                cities.id ASC")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print("MySQLdb Error: {}".format(e))
+
+    finally:
+        cur.close()
+        db.close()
