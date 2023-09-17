68045b093bf4116dfee6f16b968ea0f862eb86ad
charles otonda <charlesotonda@gmail.com>
2023-09-17 03:08:37 -0700

    a script that prints states with letter N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,23 +3,24 @@
     a script that lists all states with a name
     starting with N (upper N) from the database hbtn_0e_0_usa:
 """
-import sys
-import MySQLdb
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
 
-host = 'localhost'
+    host = 'localhost'
 
-try:
-    db = MySQLdb.connect(host=host, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        if row[1][0] == 'N':
-            print(row)
-except MySQLdb.Error as e:
-    print(f"errors {e} occured")
+    try:
+        db = MySQLdb.connect(host=host, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3])
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cur.fetchall()
+        for row in rows:
+            if row[1][0] == 'N':
+                print(row)
+    except MySQLdb.Error as e:
+        print(f"errors {e} occured")
 
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
